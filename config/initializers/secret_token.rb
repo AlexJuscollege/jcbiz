92becf4f01e5d13784c443ed984684110f03ae7a
Alex Cheung <alex.cheung.v2@gmail.com>
2013-05-28 09:50:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jcbiz::Application.config.secret_token = '2df431886ad8725786187f09994ccc8f0bd9227e83bb01756f9bbb1b6a510e5a5e5463992fbd875434583052b8cbba458c57b76a11519ad0deca952e0bd9d9a8'
